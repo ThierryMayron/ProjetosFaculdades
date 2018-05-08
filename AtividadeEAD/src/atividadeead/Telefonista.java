93913e22af71a62ec107981b9d9588da0e68ed97
ThierryMayron <38675292+ThierryMayron@users.noreply.github.com>
2018-05-08 09:29:50 -0300

    Add files via upload

---
+++ AtividadeEAD/src/atividadeead/Telefonista.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package atividadeead;
+
+/**
+ *
+ * @author thierry.mayron
+ */
+public class Telefonista extends Funcionario {
+    
+    private int codigoET;
+    
+    
+}
